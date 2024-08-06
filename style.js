823055c7e7b94e47bfa59c8260c783e8a187caed
Steven Lasso <Lasso.steven08@gmail.com>
2024-08-06 19:16:28 +0000

    JS

---
+++ style.js
@@ -0,0 +1,15 @@
+let myName = "Steven"
+
+let myNumber = 40**2 
+myNumber = myNumber - 3 
+
+let quantity = "12"; 
+quantity = Number(quantity)
+
+let isNumber = false 
+
+let userName = prompt("Aman Shah") 
+
+function handleClick() {
+    alert('Hello, ' + userName, 'my name is' + myName + '.','A lot of people say I like the number' + quantity, 'but in reality, that is' + isNumber + '.', ' In reality,  my favorite number is' + myNumber + '.');
+  }
